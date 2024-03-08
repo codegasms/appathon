3a9a06f7491a886161267ead7f90a1b14e92ab70
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-03-08 18:19:56 +0530

    Add common practices page with app localization

---
+++ farmerapp/lib/pages/subpages/settings/_common_practices.dart
@@ -0,0 +1,32 @@
+import 'package:farmerapp/localization/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class CommonPractices extends StatelessWidget {
+  const CommonPractices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Ionicons.chevron_back_outline),
+        ),
+        leadingWidth: 60,
+        title: Text(
+          AppLocalization.of(context)
+              .getTranslatedValue("common_practices")
+              .toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black54,
+          ),
+        ),
+      ),
+      body: const Text('Dummy Text'),
+    );
+  }
+}
